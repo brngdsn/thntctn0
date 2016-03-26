e65e074fa512fd7bd5ae27a7a2e9dce51d29d22d
braungoodson <braungoodson@gmail.com>
2016-03-26 16:46:23 -0400

    fixed deploy script incorrectly adding remote

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ git init
 git config --global user.email "$USER@$HOST"
 git config --global user.name $USER
 git checkout --orphan gh-pages
-    git remote add upstream https://$GH_TOKEN@github.com/brngdsn/thntctn0
+git remote add upstream "https://$GH_TOKEN@github.com/brngdsn/thntctn0.git"
 git add .
 git commit -m "deploy"
 git push upstream gh-pages --force
